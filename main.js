7902e67c5cff9904ce25cb2c5bca76aea7974891
ReadySetJoe <wolfgangwallaceband@gmail.com>
2019-09-13 17:00:41 -0400

    working prototype

--- main.js
+++ main.js
@@ -1,42 +1,76 @@
 
 
-let $displayStr =  document.querySelector('.display-text');
-let displayExpr = [];
-let notNum = false;
+let $displayStr =  document.querySelector('.display-text').textContent;
+let $elements = document.querySelectorAll('.col');
+let operationQueued = "none";
+let lastNum = 0;
+let newNum = false;
+console.log('elements', $displayStr);
 
 
-document.querySelector('.col').addEventListener("click",function() {
-  if (this.textContent == '=') {
-    evaluateDisplay();
-  } else {
-    cl = this.classList();
-    if (cl.contains('plus-minus') || ($displayStr != '0')) {
-        displayExpr.push("pm");
-        if ($displayStr[0]=="-") {
-          $displayStr.slice(1);
-        } else {
-          $displayStr.push("-");
-        }
-    } else if (cl.contains('divide')) {
-      $displayExpr.push("/");
-    } else if (!Number.isNaN(this.textContent)) {
+$elements.forEach((element) => {
+  element.addEventListener("click",function() {
+    cl = this.classList;
+    console.log(cl);
+    if (cl.contains('num')) {
+      if ($displayStr=='0' || newNum) {
+        $displayStr = this.textContent;
+      } else {
       $displayStr += this.textContent;
+      }
+    } else {
+      if (cl.contains("delete")) {
+        operationQueued = "none";
+        lastNum = 0;
+        $displayStr = '0';
+
+      } else if (cl.contains("plus-minus") && ($displayStr != '0')) {
+
+        if ($displayStr[0]=="-") {
+          $displayStr = $displayStr.slice(1);
+        } else {
+          $displayStr = "-".concat($displayStr);
+        }
+
+      } else if (cl.contains("percent")) {
+        $displayStr = (Number($displayStr)/100).toString();
+
+      } else if (cl.contains("oper")) {
+        console.log("check1: operation = " + operationQueued);
+
+        evaluateDisplay(lastNum, operationQueued, Number($displayStr));
+
+        lastNum = Number($displayStr);
+        newNum = true;
+
+        if (cl.contains("divide")) {
+          operationQueued = "/";
+        } else if (cl.contains("multiply")) {
+          operationQueued = "*";
+        } else if (cl.contains("subtract")) {
+          operationQueued = "-";
+        } else if (cl.contains("add")) {
+          operationQueued = "+";
+        } else if (cl.contains("equals")) {
+          evaluateDisplay(lastNum, operationQueued, Number($displayStr));
+        }
+
+      } else if (cl.contains("decimal") && !$displayStr.includes(".")) {
+        $displayStr += this.textContent;
+      }
     }
-    $displayExpr.push(this.textContent);
-  }
+    document.querySelector('.display-text').textContent = $displayStr;
+  });
 });
 
-evaluateDisplay(function() {
-  let out = 0;
-  let next;
-  displayExpr = displayExpr.reverse();
-  while (!(displayExpr === undefined || displayExpr.length == 0)) {
-    next = displayExpr.pop();
-    
-
-
+const evaluateDisplay = (a,oper,b) => {
+  if (oper == "+") {
+    return a + b;
+  } else if (oper == "-") {
+    return a - b;
+  } else if (oper == "*") {
+    return a * b;
+  } else if (oper == "/") {
+    return a / b;
   }
-
-
-
-});
+};
